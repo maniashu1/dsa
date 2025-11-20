d0dfe21cf577b0d46cbb479bd2abc8007263b5e6
Ashutosh Mani <ashutoshmani@Ashutoshs-MacBook-Air.local>
2025-11-20 23:28:34 +0530

    complete stl

--- STL/stl.cpp
+++ STL/stl.cpp
@@ -4,6 +4,12 @@
 #include<list>
 #include<queue>
 #include<stack>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<math.h>
+#include<algorithm>
 using namespace std;
 
 // pair -> it lies inside the utility library
@@ -246,15 +252,172 @@ void explainPriorityQueue(){
 
   // minimum heap
 
-  priority_queue<int, vector<int>, greater<int>> pq; // it kept minimum element at the top 
-  pq.push(5); // {5}
-  pq.push(2); // {2, 5}
-  pq.push(8); // {2, 5, 8}
-  pq.emplace(10); // {2, 5, 8, 10}
+  priority_queue<int, vector<int>, greater<int>> prq; // it kept minimum element at the top generally ii is also known as min heap
+  prq.push(5); // {5}
+  prq.push(2); // {2, 5}
+  prq.push(8); // {2, 5, 8}
+  prq.emplace(10); // {2, 5, 8, 10}
 
-  cout << pq.top(); // it will print 2
+  cout << prq.top(); // it will print 2
 
 }
+
+// set container conatin everything in sorted order and every elemnt is unique and everything happen in logn time complexity
+
+void explainSet(){
+  set<int> s;
+  s.insert(1);  // {1}
+  s.emplace(2);  // {1, 2}
+  s.insert(2);   // {1, 2}
+  s.insert(4);  // {1, 2, 4}
+  s.insert(3);  // {1, 2, 3, 4}
+
+  // functionality of insert in vector can be used also, they only increases effiency
+  // begin() , end() , rbegin() , rend() , size() , empty() , swap(). function are same as above
+
+  auto it = s.find(3); // it returns an iteraton which point on the 3 basically it rerturns the address of 3 
+
+  auto it = s.find(6); // if element is not in the set it will always return s.end() i.e the iterator which points to the end 
+
+  s.erase(3) ; // it erases 3 from the set and it will take logarithmic time 
+
+  int cnt = s.count(1); // if it exists it will give 1 otherwise it give 0
+
+  auto it = s.find(3);
+  s.erase(it) ; // it will take constant time // we can also do that s.erase(s1 , s2);
+
+  // lower_bound() and upper_bound() function works in the same way as in vector it does 
+
+  auto it = s.lower_bound(2);
+
+  auto it = s.upper_bound(3);
+  
+}
+
+// Multiset is similar to set it also contain element in sorted manner but not unique i.e duplicate is may present in the set
+
+void explainMultiset(){
+
+  multiset<int> ms;
+  ms.insert(1); // {1}
+  ms.insert(1);  // {1, 1}
+  ms.insert(1); // {1, 1, 1}
+  ms.erase(1); // all 1's erased...
+
+  int cnt = ms.count(1) ; // count the no of 1's present in the multiset 
+
+  ms.erase(ms.find(1)) ; // it will erase only one 1's by finding and erasing on the only particular addresses
+
+  // ms.erase(ms.find(1) , ms.find(1)+2); // it will erase 2 ones..
+
+  // rest all function same as set
+
+}
+
+// Unordered set i.e element is stored in randomised ordered 
+// lower_bound and upper_bound function doesn't work here 
+// rest all function are same as above
+// it does not stores in any particular order it has a better complexity than set in most cases except when collisons happen
+// time complexity is O(1) // but in worst case it will goes O(n)
+
+void explainUnorderedset(){
+  unordered_set<int> us;
+  
+}
+
+
+// map
+// it stores data in the key value pair form 
+// it can store any data type like integer , double float etyc...
+
+void explainMap(){
+  map<int , int> mpp;
+  
+  map<int, pair<int , int>> mpp;
+
+  map<pair<int,int>, int> mpp;
+
+  mpp[1] = 2; // it eill store like key 1 value 2
+
+ // mpp.emplace({3 , 1}); // it will store like key as 3 and value at 3 is 1
+
+  mpp.insert({2,4}) ; // it will also stored in the same manner 
+
+  mpp[{2,3}] = 10; 
+
+  // {
+  //   {1,2}
+  //   {2,4}
+  //   {3,1}
+  // }
+  
+  for(auto it : mpp) {
+    cout << it.first << " " << it.second << endl ; // if something is not exit it will print either 0 or null
+  }
+
+  cout << mpp[1];
+  cout << mpp[5];
+
+  // auto it = mpp.find(3);
+  // cout << *(it).second;
+
+  auto it = mpp.find(5);
+  
+  auto it = mpp.lower_bound(2);
+  auto it = mpp.upper_bound(3);
+  // all other function are same as above 
+}
+
+// multimap
+
+void explainMultimap(){
+  // everything is similar to map, only thing it can stor multiple keys
+  // only mpp[key] can not be used here
+
+}
+// unorderd_map same as set and unorderd_set but not in sorted and have unique keys and tc is constant
+// comp function
+bool comp(pair<int, int> p1 , pair<int , int> p2){
+  if(p1.second < p2.second) return true;
+  if(p1.second > p2.second) return false; // then it will do the internal swapping
+  // if they are same i.e p1.second == p2.second 
+  if(p1.first > p2.first) return true;
+  return false; // please swap it 
+}
+
+void explainExtra(){
+    // sort(a , a + n);
+    // sort(v.begin() , v.end()) ;
+    // sort(a+2, a+4);
+    // sort(a , a+n, greater<int>) ; // it will sort them in descending order
+
+    pair<int , int> a[] = {{1,2}, {2,1}, {4,1}};
+    // sort it according to second element 
+    // if secod element is same , then sort according to first element but in descinding
+
+   //  sort(a , a+n , comp);
+   
+
+   int num = 7;  //7 means 00000111 -> in binary 
+   //int cnt = __builtin_popcount(); // it count 3 bits of 1 it count no of set bits 
+
+   long long num = 65657215471254721;
+  // int cnt = __builtin_popcountll(); 
+
+  // string
+
+  string s = "1234";
+  sort(s.begin() , s.end());
+
+  do{
+    cout << s << endl;
+  } while(next_permutation(s.begin() , s.end()));
+  
+  // int maxi = *max_element(a,a+n);
+
+   
+}
+
 
 
 int main(){
@@ -271,8 +434,15 @@ int main(){
 
   // explainQueue();
 
-   explainPriorityQueue();
+   // explainPriorityQueue();
 
+   // explainSet();
+
+   // explainMultiset();
+   
+   // explainUnorderedset();
+
+   // explainMap();
     return 0;
 }
 

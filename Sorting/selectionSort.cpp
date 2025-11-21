bfd43bd9885e6ac83ac9ba7c7d76aeeff98a1096
maniashu1 <maniashu1a2b@gmail.com>
2025-11-21 22:19:34 +0530

    selection sort

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -3,10 +3,41 @@
 using namespace std;
 
 void selectionSort(vector<int> &arr , int n){
-   
+   for(int i=0; i<n-1; i++){
+    int min = INT_MAX;
+    int minIdx = -1;
+    // minimum element calculation 
+    for(int j=i; j<n; j++){
+        if(arr[j] < min){
+            min = arr[j];
+            minIdx = j;
+        }
+    }
+    swap(arr[i], arr[minIdx]);
+   }
 }
 
 int main(){
+
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter the element of the array :";
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Sorted array : ";
+
+    selectionSort(arr , n);
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }

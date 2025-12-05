67dd4cf73cbc7ab2ac5e475087d928805b409cac
maniashu1 <maniashu1a2b@gmail.com>
2025-12-05 22:44:41 +0530

    move zero to end

--- Array/moveZtoend.cpp
+++ Array/moveZtoend.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 
 void moveZerotoend(int arr[] , int n){
-   for(int i=0; i<n; i++){
-    if(arr[i] == 0 && arr[i+1] > 0){
-        swap(arr[i] , arr[i+1]);
+   int i = 0;
+   int j = 1;
+   while(j < n){
+    if(arr[i] == 0 && arr[j] != 0){
+        swap(arr[i] , arr[j]);
+    } else if(arr[i] != 0){
+        i++;
+        j++;
+    } else if(arr[i] == 0 && arr[j] == 0){
+        j++;
     }
    }
 }

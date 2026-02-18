2cbe362bc9ef997012ee1c8fd4234441da09a791
maniashu1 <maniashu1a2b@gmail.com>
2026-02-18 22:49:20 +0530

    Checking if array pairs is divisible by k

---
+++ SetsNdMap/ArrayPairDivByK.cpp
@@ -0,0 +1,31 @@
+// Leetcode 1497 ( checking if array pairs is divisible by k )
+
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+ bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int,int> mp;
+        for(int ele : arr){
+            ele = ((ele%k)+k)%k ;
+            mp[ele]++;
+        }
+        if(mp.find(0)!=mp.end()){
+            if(mp[0]%2!=0) return false;
+            mp.erase(0);
+        }
+
+        for(auto x : mp){
+            int ele = x.first;
+            int rem = k - ele;
+            if(mp.find(rem) == mp.end()) return false;
+            if(mp[ele] != mp[rem]) return false;
+        }
+        return true;
+    }
+
+    int main(){
+
+        return 0;
+    }

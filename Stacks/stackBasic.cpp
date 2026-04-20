3305ccc801708e3a40900920a12a6b2b100f14dd
maniashu07 <maniashu1a2b@gmail.com>
2026-04-20 13:56:57 +0530

    Stack basics

---
+++ Stacks/stackBasic.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main (){
+stack<int> st;
+st.push(10);
+st.push(20);
+st.push(30);
+st.push(40);
+cout << st.size() << endl;
+
+// printing the element of the stack but stack get also empty
+// while(st.size() > 0){
+//     cout << st.top() << " ";
+//     st.pop();
+// }
+
+// printing the element of the stack and putback to the stack
+stack<int> temp;
+while(st.size() > 0){
+    cout << st.top() << " ";
+    int x = st.top();
+    st.pop();
+    temp.push(x);
+}
+while(temp.size() > 0){
+    int x = temp.top();
+    temp.pop();
+    st.push(x);
+}
+
+// printing the element of the stack in same order
+
+stack<int> temp2;
+// psuhing all the element on other stack
+while(st.size() > 0){
+    int x = st.top();
+    temp2.push(x);
+    st.pop();
+}
+cout << endl;
+// now print the element of the stack while poping and psuhing into the original stack
+while(temp2.size() > 0){
+    cout << temp2.top() << " ";
+    int x = temp2.top();
+    temp2.pop();
+    st.push(x);
+}
+    return 0;
+}

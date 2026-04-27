88f4a2fe03d6674e8b3fd46a7fdc2f2a9f9b5ef8
maniashu07 <maniashu1a2b@gmail.com>
2026-04-27 19:39:59 +0530

    Postfix to Infix conversion

---
+++ Stacks/PostfixToInfix.cpp
@@ -0,0 +1,44 @@
+
+// Postfix expression evalution using single stack
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+string solve(string val1 , string val2 , char ch){
+    string s;
+    s += val1;
+    s.push_back(ch);
+    s += val2;
+    return s;
+}
+
+
+int main(){
+
+  string s = "79+4*8/3-";
+
+  // we need only one stack
+
+  stack<string> val;
+
+  for(int i=0; i<s.length(); i++){
+    // checking if s[i] is a digit ( 0 - 9 )
+    if(s[i] >= 48 && s[i] <= 57){
+        val.push(to_string(s[i] - '0'));    // converting character to integer before pushing 
+    } else {
+        string val2 = val.top();
+        val.pop();
+        string val1 = val.top();
+        val.pop();
+        string ans = solve(val1 , val2 , s[i]);
+        val.push(ans);
+      }
+    }
+  
+
+    cout << val.top();
+
+    return 0;
+}

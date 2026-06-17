d2a94b7cda36fb77d4959c00b8b21a6cf0ae1574
maniashu07 <maniashu1a2b@gmail.com>
2026-06-17 19:47:11 +0530

    Minimum path sum space optimised solution

--- Dynamic Programming/MinimumPathSum.cpp
+++ Dynamic Programming/MinimumPathSum.cpp
@@ -35,6 +35,35 @@ public:
     }
 };
 
+// Solution with using no extra space 
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        // fill the first row completely 
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+        
+        // fill the first col completely
+        for (int j = 1; j < m; j++) {
+            grid[j][0] += grid[j - 1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i][j] + min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+};
+
 int main(){
 
     return 0;

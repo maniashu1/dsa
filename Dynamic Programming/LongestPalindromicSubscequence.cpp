700818638007b1e9c0787f3b372c196acbb301ca
maniashu07 <maniashu1a2b@gmail.com>
2026-06-22 12:39:08 +0530

    Longest palindromic subscequence

---
+++ Dynamic Programming/LongestPalindromicSubscequence.cpp
@@ -0,0 +1,44 @@
+
+// Leetcode 516 ( Longest palindromic subsequence ) Medium
+
+// logic apply lcs on s and s rev
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+   
+    vector<vector<int>> dp;
+
+    int lcsPalindrome(string&s, string& srev , int i , int j){
+        if(i >= s.size()) return 0;
+        if(j >= srev.size()) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        // take both
+        if(s[i] == srev[j]) {
+            return dp[i][j] = 1 + lcsPalindrome(s,srev,i+1,j+1);
+        } else {
+            // include s[i] and exclude srev[j]
+            // include srev[j] and exclude s[i]
+            return dp[i][j] = max(lcsPalindrome(s,srev,i+1,j) , lcsPalindrome(s,srev,i,j+1));
+        }
+        return dp[i][j];
+    }
+
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        dp.clear();
+        dp.resize(n,vector<int>(n+5,-1));
+        string srev = s;
+        reverse(srev.begin(),srev.end());
+        return lcsPalindrome(s,srev,0,0);
+    }
+};
+
+int main(){
+
+    return 0;
+}

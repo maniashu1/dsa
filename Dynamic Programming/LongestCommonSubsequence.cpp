13ac49a45c301c19d99d5cdb6ad2f1261b58b79e
maniashu07 <maniashu1a2b@gmail.com>
2026-06-23 18:54:00 +0530

    Printing length of longest substring

--- Dynamic Programming/LongestCommonSubsequence.cpp
+++ Dynamic Programming/LongestCommonSubsequence.cpp
@@ -7,6 +7,8 @@
 #include<vector>
 using namespace std;
 
+// recursion and dp memoization method 
+
 class Solution {
 public:
     vector<vector<int>> dp;  // 2D dp vector bcoz here state of dp is 2 i.e, i and j both are changing
@@ -50,6 +52,28 @@ public:
             }
         } 
         return dp[m][n];
+
+    }
+
+    // Printing length of longest substring 
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+        vector<vector<int>> dp;
+        dp.assign(m+1,vector<int>(n+1,0));
+        int ans = INT_MIN;
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(text1[i-1] == text2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 0;
+                    ans = max(ans,dp[i][j]);
+                }
+            }
+        } 
+        return max;
 
     }
 

532cfd08b4109f9c6b51eb8a7edd9572f25268fc
maniashu07 <maniashu1a2b@gmail.com>
2026-07-18 12:53:05 +0530

    Finding peak element using bonary search

--- BinarySearch/FindingPeakElement.cpp
+++ BinarySearch/FindingPeakElement.cpp
@@ -30,6 +30,36 @@ public:
     }
 };
 
+
+class Solution {
+public:
+
+    // Optimised Binary search approach . T.C -> O(log n)
+
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return 0; // if array has only one element
+        if(nums[0] > nums[1]) return 0; // First element is a peak element 
+        if(nums[n-1] > nums[n-2]) return n-1; // last element is a peak element 
+        int lo = 1;
+        int hi = n-2;
+        int mid = -1;
+        while(lo <= hi){
+            mid = lo + (hi-lo)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){  // mid is a peak element 
+                return mid;
+            }
+            if(nums[mid] > nums[mid-1]){  // peak is in right side ans mid is on increasing curve 
+                lo = mid+1;
+            } else {
+                hi = mid-1;
+            }
+        }
+        return 0;
+        
+    }
+};
+
 int main(){
 
     return 0;
